35c532e814d44f7c204fc21b391d0119df3b2db2
S-Venkata-Vasanth <vasanth@soomuch.ai>
2024-11-12 19:18:07 +0530

    Updated fortunes and challenges

---
+++ fortunes.js
@@ -0,0 +1,15 @@
+const fortunes = [
+    "Today will be a lucky day!",
+    "You will meet someone interesting soon.",
+    "Good news is coming your way!",
+    "Adventure awaits you.",
+    "You’ll make a positive change today."
+];
+
+const challenges = [
+    "Compliment a stranger today!",
+    "Take a 10-minute walk and enjoy nature.",
+    "Write down three things you’re grateful for.",
+    "Try something new for lunch.",
+    "Spend 5 minutes meditating."
+];
